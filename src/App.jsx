da4cc5bba1803d6c4b0d5ad8fac7a1c317c6def4
Neha <nehad2000@gmail.com>
2023-11-07 13:57:19 -0600

    working on fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import ContactList from './components/ContactList';
 
 function App() {
 
   return (
     <>
-     
+     <ContactList/>
     </>
   )
 }
